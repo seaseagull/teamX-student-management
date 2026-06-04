2bb538529b6e03516a8b70d268644ce3118eb245
Hu <2586215773@qq.com>
2026-06-04 22:52:38 +0800

    校车查询和离校申请fxml

--- java-server/src/main/java/cn/edu/sdu/java/server/repositorys/DormAllotRepository.java
+++
@@ -1,6 +0,0 @@
-package cn.edu.sdu.java.server.repositorys;
-
-import cn.edu.sdu.java.server.models.DormAllot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DormAllotRepository extends JpaRepository<DormAllot, Long> {}
