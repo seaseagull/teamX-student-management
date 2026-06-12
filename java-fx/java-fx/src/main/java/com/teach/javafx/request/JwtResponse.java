1fc8e8df68d4aa2a605d4e512f7f46889949c028
Hu <2586215773@qq.com>
2026-06-12 22:32:22 +0800

    校园公告

--- java-fx/java-fx/src/main/java/com/teach/javafx/request/JwtResponse.java
+++ java-fx/java-fx/src/main/java/com/teach/javafx/request/JwtResponse.java
@@ -1,29 +1,22 @@
 package com.teach.javafx.request;
+
 /**
  * JwtResponse JWT数据返回对象 包含客户登录的信息
  * String tokenType token字符串
  * Integer id 用户的ID user_id
  * String username 用户的登录名
- * String accessToken 登录客户加密数据串 请求是要传到后端进行权限验证
- * String roles 用户角色 ROLE_ADMIN, ROLE_STUDENT, ROLE_TEACHER
+ * String token 登录客户加密数据串 请求是要传到后端进行权限验证
+ * String role 用户角色 ROLE_ADMIN, ROLE_STUDENT, ROLE_TEACHER
+ * String perName 用户姓名
  */
 
 public class JwtResponse {
     private String tokenType;
     private Integer id;
     private String username;
-    private String perName;
     private String token;
     private String role;
     private String perName;
-
-    public String getPerName() {
-        return perName;
-    }
-
-    public void setPerName(String perName) {
-        this.perName = perName;
-    }
 
     public String getTokenType() {
         return tokenType;
@@ -49,14 +42,6 @@ public class JwtResponse {
         this.username = username;
     }
 
-    public String getPerName() {
-        return perName;
-    }
-
-    public void setPerName(String perName) {
-        this.perName = perName;
-    }
-
     public String getToken() {
         return token;
     }
@@ -71,5 +56,13 @@ public class JwtResponse {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getPerName() {
+        return perName;
+    }
+
+    public void setPerName(String perName) {
+        this.perName = perName;
     }
 }
